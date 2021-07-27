72423a404d5d79801d080ecba7527d18f7be9b64
milad heidari <milad.heidari.dev@gmail.com>
2021-07-27 17:51:05 +0430

    add css code for AddTodoFormLayout.jsx

--- src/Layout/TodoFormLayout/AddTodoFormLayout.jsx
+++ src/Layout/TodoFormLayout/AddTodoFormLayout.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import './addTodoFormLayout.css'
 function AddTodoForm() {
     return (
-        <div>
+        <div className="container">
             
         </div>
     )
