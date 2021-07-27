e1c791f82473b2e627a61f15309436522a970f5e
milad heidari <milad.heidari.dev@gmail.com>
2021-07-27 16:55:48 +0430

    use proptypes for Input.jsx component

--- src/Components/Partials/Input.jsx
+++ src/Components/Partials/Input.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function Input() {
+
+
+
+function Input({className,type,id,placeholder,formikGetFieldProps}) {
     return (
         <input
-            
+            className={className}
+            type={type}
+            id={id}
+            placeholder={placeholder}
+            {...formikGetFieldProps}
         />
-        
     )
+}
+
+Input.propTypes = {
+    className: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    ids:PropTypes.string,
+    placeholder:PropTypes.string.isRequired,
+    formikGetFieldProps:PropTypes.object.isRequired
 }
 
 export default Input
