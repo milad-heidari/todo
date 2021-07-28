4373de475739a02e340d0ce7d80ce5029a5dcb4b
milad heidari <milad.heidari.dev@gmail.com>
2021-07-28 18:56:43 +0430

    write css className and css code

--- src/Layout/TodoFormLayout/AddTodoFormLayout.jsx
+++ src/Layout/TodoFormLayout/AddTodoFormLayout.jsx
@@ -8,9 +8,31 @@ import components from '../../Components/Partials/PatialsExport'
 function AddTodoForm() {
     return (
         <div className="container">
-            <components.Input
-                
-            />
+            <div className="add-form-box" >
+                <components.P
+                    className="add-form-box__title"
+                    text="Add New Task"
+                />
+                <components.Form
+                    className="add-form"
+                >
+
+                    <components.Input
+                        className="add-form__input"
+                        type="text"
+                        placeholder="write new task"
+                        id=""
+                        formikGetFieldProps={{}}
+                        key=""
+                    />
+
+                    <components.Button
+                        className="add-form__button"
+                        title="Add Task"
+                        type="submit"
+                    />
+                </components.Form>
+            </div>
         </div>
     )
 }
