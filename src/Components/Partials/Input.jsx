e3144225aee195ba7225759f623e4cbf3d8b169e
milad heidari <milad.heidari.dev@gmail.com>
2021-07-28 11:25:55 +0430

    fix some code

--- src/Components/Partials/Input.jsx
+++ src/Components/Partials/Input.jsx
@@ -19,9 +19,9 @@ function Input({className,type,id,placeholder,formikGetFieldProps}) {
 Input.propTypes = {
     className: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
-    ids:PropTypes.string,
     placeholder:PropTypes.string.isRequired,
-    formikGetFieldProps:PropTypes.object.isRequired
+    formikGetFieldProps:PropTypes.object.isRequired,
+    id:PropTypes.string,
 }
 
 export default Input
