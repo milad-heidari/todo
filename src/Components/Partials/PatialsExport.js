fb4fa4fedce77419b350a794aa4dfc7d6bc4d3f8
milad heidari <milad.heidari.dev@gmail.com>
2021-07-28 12:25:26 +0430

    add Form.jsx to export file

--- src/Components/Partials/PatialsExport.js
+++ src/Components/Partials/PatialsExport.js
@@ -1,10 +1,11 @@
 import Button from './Button'
 import Input from './Input'
 import P from './P'
-
+import Form from './Form'
 const components = {
     Button,
     Input,
     P,
+    Form,
 }
 export default components;
