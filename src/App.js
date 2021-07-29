bebc3108d77d221509a96c4f33cdc2340b584053
milad heidari <milad.heidari.dev@gmail.com>
2021-07-29 14:06:47 +0430

    import Lists and fix css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import List from './Layout/List/List'
+import "./app.css"
 import AddTodoFormLayout from './Layout/TodoFormLayout/AddTodoFormLayout'
 function App() {
   return (
     <div className="App">
       <AddTodoFormLayout/>
+      <div className="lists-container">
+        <List ListTitle="Todo List" className="lists-container__items">
+
+        </List>
+
+        <List ListTitle="Doing List" className="lists-container__items">
+
+        </List>
+        
+        <List ListTitle="Done List" className="lists-container__items">
+
+        </List>
+
+      </div>
     </div>
   );
 }
