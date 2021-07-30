67f5edc54000e8e589bf5e74e6b6088425598396
milad heidari <milad.heidari.dev@gmail.com>
2021-07-30 14:28:50 +0430

    import and use MainLayout.jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import List from './Components/List/List'
 import "./app.css"
-import AddTodoFormComponent from './Components/TodoFormComponent/AddTodoFormComponent'
+import MainLayout from './Layout/MainLayout'
 function App() {
   return (
     <div className="App">
-      <AddTodoFormComponent/>
-      <div className="lists-container">
-        <List ListTitle="Todo List" className="lists-container__items">
-
-        </List>
-
-        <List ListTitle="Doing List" className="lists-container__items">
-
-        </List>
-        
-        <List ListTitle="Done List" className="lists-container__items">
-
-        </List>
-
-      </div>
+      <MainLayout/>
+      
     </div>
   );
 }
