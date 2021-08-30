d9f056305f9ebdd8891ac6f8d57c957cb08e01a4
milad heidari <milad.heidari.dev@gmail.com>
2021-08-31 00:50:27 +0430

    fix code

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,38 +1,31 @@
-import React from 'react'
-import List from '../Components/List/List'
-import TodoFormComponent from '../Components/TodoFormComponent/AddTodoFormComponent'
+import React from "react";
+import List from "../Components/List/List";
+import TodoFormComponent from "../Components/TodoFormComponent/AddTodoFormComponent";
 function MainLayout() {
-    return (
-        <>
-            <TodoFormComponent/>
-            <div className="lists-container">
-        <List 
-            ListTitle="Todo List" 
-            className="lists-container__items todo-background-color"
-            pClassName="list-title"
-            >
+  return (
+    <>
+      <TodoFormComponent />
+      <div className="lists-container">
+        <List
+          ListTitle="Todo List"
+          className="lists-container__items todo-background-color"
+          pClassName="list-title"
+        ></List>
 
-        </List>
+        <List
+          ListTitle="Doing List"
+          className="lists-container__items doing-background-color"
+          pClassName="list-title"
+        ></List>
 
-        <List 
-            ListTitle="Doing List" 
-            className="lists-container__items doing-background-color"
-            pClassName="list-title"
-            >
-
-        </List>
-        
-        <List 
-            ListTitle="Done List" 
-            className="lists-container__items done-background-color"
-            pClassName="list-title"
-            >
-
-        </List>
-
+        <List
+          ListTitle="Done List"
+          className="lists-container__items done-background-color"
+          pClassName="list-title"
+        ></List>
       </div>
-        </>
-    )
+    </>
+  );
 }
 
-export default MainLayout
+export default MainLayout;
