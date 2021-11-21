805f1e5fe682020163c77b56d23e7f573dcf3402
milad heidari <milad.heidari.dev@gmail.com>
2021-11-21 18:41:47 +0330

    add redux store

---
+++ src/Store/reducer.js
@@ -0,0 +1,34 @@
+const initState = {
+    tasks:[],
+    modalVisiblity:false,
+    dataForModal:{}
+}
+
+const reducer = (state= initState,action)=>{
+    let newState = state
+
+    switch (action.type) {
+        case "ADD_TASK":
+            newState = {...state,tasks:[...state.tasks,action.payload]}
+            break;
+        case "UPDATED_TASKS":
+            newState = {...state,tasks:action.payload}
+            break;
+            case "GET_INIT_DATA":
+            console.log('INIT',action.payload)
+            newState = {...state,tasks:action.payload}
+            break;
+            case 'SET_MODAL_VISIBILITY':
+                newState = {...state,modalVisiblity:action.payload}
+            break;
+        case "SET_DATA_FOR_MODAL":
+                newState = {...state,dataForModal:action.payload}
+            break;
+        default:
+            break;
+    }
+    // console.log('INIdfasdfaT',newState)
+    return newState
+}
+
+export default reducer
