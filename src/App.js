a7a6b3f3a243a4ef211bd1cd7fb359fc4cf232cf
milad heidari <milad.heidari.dev@gmail.com>
2021-11-21 18:39:04 +0330

    import MainLayout.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React from 'react'
+import {Provider} from 'react-redux'
+import store from './Store/index'
 import "./App.css"
 import MainLayout from './Layout/MainLayout'
+// import {StateManager} from './Store/TodoContext'
 function App() {
+
   return (
-    <div className="App">
-      <MainLayout/>
-      
-    </div>
+
+    <Provider store={store}>
+
+      <div className="App">
+        <MainLayout />
+      </div>
+    </Provider>
+
+    
   );
 }
 
