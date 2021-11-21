91b8c2460d4c216ba114c1ac6e1e3584f3210e5f
milad heidari <milad.heidari.dev@gmail.com>
2021-11-21 18:36:42 +0330

    omplete List and ListItem components

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -1,24 +1,56 @@
-import React from 'react'
-import "./list.css"
-import PropTypes from 'prop-types'
-import elements from '../../Elements/ExportElements'
-function List({text,className,children,pClassName}) {
-    return (
-        <div className={className}>
-            {/* <elements.P
-                className={pClassName}
-                text={text}
+import React from "react";
+import "./list.css";
+import PropTypes from "prop-types";
+import elements from "../../Elements/ExportElements";
+import ListItem from "../List/ListItem";
 
-            /> */}
-            {children}
-        </div>
-    )
+function List({ tasks, className, children, listType }) {
+  // return (
+  //   <ListItem data={task} key={index} />
+  // );
+console.log('ttttttttttt',tasks)
+  let filteredTasks = []
+  
+  if (tasks.length >= 1) {
+     filteredTasks = tasks.filter((task) => {
+        if (task.taskStatus === listType) {
+          return task
+        }else{return null}
+      })
+    }
+    // else{
+    //   return null
+    // }
+    // console.log(listType,filteredTasks)
+
+  const renderList = (filteredTasks) => {
+    if (filteredTasks.length >= 1) {
+      // console.log('filter',filteredTasks)
+     return filteredTasks.map((task, index) => {
+        return <ListItem data={task} key={index} />;
+      });
+    } else {
+      return (
+        <elements.P
+          text={`your ${listType} task list is empty`}
+          className="list-title__item"
+        />
+      );
+    }
+  };
+  return (
+    <elements.Div className={className}>
+      {children}
+      {renderList(filteredTasks)}
+    </elements.Div>
+  );
 }
 
 List.propTypes = {
-    className:PropTypes.string,
-    pClassName:PropTypes.string,
-    text:PropTypes.string.isRequired
-}
+  className: PropTypes.string,
+  children: PropTypes.element,
+  tasks: PropTypes.array.isRequired,
+  listType: PropTypes.string.isRequired,
+};
 
-export default List
+export default List;
--- src/Components/List/ListItem.jsx
+++ src/Components/List/ListItem.jsx
@@ -1,17 +1,50 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import elements from "../../Elements/ExportElements"
+function ListItem({data,setModalVisiblity,setDataForModal}) {
 
-function ListItem({className,pClassName,text,children}) {
     return (
-<div className={className}>
-            {/* <elements.P
-                className={pClassName}
-                text={text}
-            /> */}
-            {children}
 
-        </div>
+        <elements.Div 
+        onClickHandler={()=>{
+          setModalVisiblity(true)
+          setDataForModal(data)
+        }
+          } className="list-item">
+
+          <elements.P text={data.title} className="list-title__item" />
+          <elements.P text="click for more options" className="list-title__item-hover-text" />
+    
+        </elements.Div>
+
     )
 }
 
-export default ListItem
+const mapState = (state)=>{
+    return {
+        modalVisiblity :state.modalVisiblity
+    }
+}
+
+const mapDispatch = (dispatch)=>{
+    return {
+      setModalVisiblity : (payload)=>{
+        dispatch(
+          {
+            type:'SET_MODAL_VISIBILITY',
+            payload
+          }
+        )
+      },
+      setDataForModal : (payload)=>{
+        dispatch(
+          {
+            type:"SET_DATA_FOR_MODAL",
+            payload
+          }
+        )
+      }
+    }
+  }
+
+export default connect(mapState,mapDispatch)(ListItem)
