7e2ccc22ef46bc96165093a5afb9ecfbd759cc92
milad heidari <milad.heidari.dev@gmail.com>
2021-11-21 18:33:48 +0330

    import connect and complete component

--- src/Components/TodoFormComponent/AddTodoFormComponent.jsx
+++ src/Components/TodoFormComponent/AddTodoFormComponent.jsx
@@ -1,39 +1,78 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import "./addTodoFormComponent.css"
 import elements from '../../Elements/ExportElements'
+import {useFormik} from 'formik'
 
-function AddTodoForm() {
+function AddTodoForm({addNewTask}) {
+
+    const currentValues = JSON.parse(localStorage.getItem('tasks'))
+
+
+    const formik = useFormik({
+        initialValues:{
+            title:''
+        },
+        onSubmit: values =>{
+            if (currentValues) {
+                const newValue = {id:Math.round(Math.random()*10000),title:values.title,taskStatus:"todo"}
+                addNewTask(newValue)
+                const updatedValue = [...currentValues,newValue]
+                localStorage.setItem("tasks",JSON.stringify(updatedValue))
+
+            }else{
+                const newValue = {id:Math.round(Math.random()*10000),title:values.title,taskStatus:"todo"}
+                addNewTask(newValue)
+                localStorage.setItem('tasks',JSON.stringify([newValue]))
+            }
+            // console.log('valll',values)
+        }
+    })
+
     return (
         <div className="container">
             <div className="add-form-box" >
                 <elements.P
                     className="add-form-box__title"
                     text="Add New Task"
-                />
+                    />
                 <elements.Form
                     className="add-form"
-                    onSubmit={()=>{}}
-                >
+                    onSubmit={formik.handleSubmit}
+                    >
 
                     <elements.Input
                         className="add-form__input"
                         type="text"
                         placeholder="write new task"
-                        id=""
-                        formikGetFieldProps={{}}
+                        inputValue={formik.values.title}
+                        id="title"
+                        onChange={formik.handleChange}
+                        // formikGetFieldProps={{}}
                         key=""
-                    />
+                        />
 
                     <elements.Button
-                        clickHandler={(e)=>{alert('clicked')}}
+                        clickHandler={()=>{}}
                         className="add-form__button"
                         title="Add Task"
                         type="submit"
-                    />
+                        />
                 </elements.Form>
             </div>
         </div>
     )
 }
 
-export default AddTodoForm
+const mapDispatch = (dispatch)=>{
+    return {
+        addNewTask : (payload)=>{
+            dispatch({
+                type:'ADD_TASK',
+                payload
+            })
+        }
+    }
+}
+
+export default connect(null,mapDispatch)(AddTodoForm)
