a3b7912c8d9e00ce660e5214b703765f9a89675b
milad heidari <milad.heidari.dev@gmail.com>
2022-09-03 11:39:39 +0430

    format docs

--- src/Components/List/ListItem.jsx
+++ src/Components/List/ListItem.jsx
@@ -1,50 +1,47 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import elements from "../../Elements/ExportElements"
-function ListItem({data,setModalVisiblity,setDataForModal}) {
-
-    return (
-
-        <elements.Div 
-        onClickHandler={()=>{
-          setModalVisiblity(true)
-          setDataForModal(data)
-        }
-          } className="list-item">
-
-          <elements.P text={data.title} className="list-title__item" />
-          <elements.P text="click for more options" className="list-title__item-hover-text" />
-    
-        </elements.Div>
-
-    )
+import React from "react";
+import { connect } from "react-redux";
+import elements from "../../Elements/ExportElements";
+function ListItem({ data, setModalVisiblity, setDataForModal }) {
+  return (
+    <elements.Div
+      onClickHandler={() => {
+        setModalVisiblity(true);
+        setDataForModal(data);
+      }}
+      className="list-item"
+    >
+      <elements.P 
+        text={data.title} 
+        className="list-title__item" />
+      <elements.P
+        text="click for more options"
+        className="list-title__item-hover-text"
+      />
+    </elements.Div>
+  );
 }
 
-const mapState = (state)=>{
-    return {
-        modalVisiblity :state.modalVisiblity
-    }
-}
+const mapState = (state) => {
+  return {
+    modalVisiblity: state.modalVisiblity,
+  };
+};
 
-const mapDispatch = (dispatch)=>{
-    return {
-      setModalVisiblity : (payload)=>{
-        dispatch(
-          {
-            type:'SET_MODAL_VISIBILITY',
-            payload
-          }
-        )
-      },
-      setDataForModal : (payload)=>{
-        dispatch(
-          {
-            type:"SET_DATA_FOR_MODAL",
-            payload
-          }
-        )
-      }
-    }
-  }
+const mapDispatch = (dispatch) => {
+  return {
+    setModalVisiblity: (payload) => {
+      dispatch({
+        type: "SET_MODAL_VISIBILITY",
+        payload,
+      });
+    },
+    setDataForModal: (payload) => {
+      dispatch({
+        type: "SET_DATA_FOR_MODAL",
+        payload,
+      });
+    },
+  };
+};
 
-export default connect(mapState,mapDispatch)(ListItem)
+export default connect(mapState, mapDispatch)(ListItem);
